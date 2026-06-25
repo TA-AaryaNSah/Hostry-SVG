2727eb9679cc26e2e86c4885b9fedb3e674214cb
Aaryan Sah <167690903+TA-AaryaNSah@users.noreply.github.com>
2026-06-26 01:10:23 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,13 +5,29 @@ MONGO_URL = "mongodb+srv://asvm:incorrectasvm@cluster0.v2z8vnw.mongodb.net/?appN
 
 client = AsyncIOMotorClient(MONGO_URL)
 db = client["FileStreamBot"]
-collection = db["files"]
+files_col = db["files"]
+users_col = db["users"]
 
+# ---- FILE FUNCTIONS ----
 async def save_file(unique_id, message_id):
-    await collection.insert_one({"_id": unique_id, "message_id": message_id})
+    await files_col.insert_one({"_id": unique_id, "message_id": message_id})
 
 async def get_file(unique_id):
-    result = await collection.find_one({"_id": unique_id})
+    result = await files_col.find_one({"_id": unique_id})
     if result:
         return result.get("message_id")
     return None
+
+# ---- USER FUNCTIONS ----
+async def add_user(user_id):
+    if not await users_col.find_one({"_id": user_id}):
+        await users_col.insert_one({"_id": user_id})
+
+async def get_all_users():
+    return await users_col.find().to_list(length=None)
+
+# ---- STATS FUNCTION ----
+async def get_stats():
+    total_users = await users_col.count_documents({})
+    total_files = await files_col.count_documents({})
+    return total_users, total_files
